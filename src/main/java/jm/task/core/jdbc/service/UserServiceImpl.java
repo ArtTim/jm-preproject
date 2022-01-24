d4d4a12a4744ed1a1dacff94f56a9dd2c47bc46c
tim-murr@inxo.ru <Daemonter89>
2022-01-24 23:55:09 +0300

    Added createUsersTable & dropUsersTable

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,16 +1,39 @@
 package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
     public void createUsersTable() {
-
+        Util connect = new Util();
+        String query = "CREATE TABLE Users (" +
+                "  `id` INT NOT NULL AUTO_INCREMENT," +
+                "  `name` VARCHAR(45) NOT NULL," +
+                "  `last_name` VARCHAR(45) NULL," +
+                "  `age` INT NOT NULL," +
+                "  PRIMARY KEY (`id`));";
+        try {
+            Statement statement = connect.getConnection().createStatement();
+//             System.out.println(statement.executeQuery(checkQuery));
+            statement.executeUpdate(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
-
+        Util connect = new Util();
+        try {
+            Statement statement = connect.getConnection().createStatement();
+            statement.executeUpdate("DROP TABLE Users");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,23 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private Connection connection;
+
+    public Util() {
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/first_db", "root", "admin");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
 }
